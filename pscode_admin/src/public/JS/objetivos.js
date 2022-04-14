12ff41f33ba0032333b837823a8a290d68e86a08
Zaida88 <zsm.mejia@yavirac.edu.ec>
2022-04-14 17:58:44 -0500

    realizacion de cambios en estilos

--- pscode_admin/src/public/JS/objetivos.js
+++ pscode_admin/src/public/JS/objetivos.js
@@ -7,7 +7,7 @@ class objetivo{
         for(let i = 0; i<parseInt(this.numero.value); i++){
         let input = document.createElement('textarea')
         input.setAttribute('type', 'text')
-        input.name = 'objetivos'
+        input.name = 'objetivos' 
         this.contenedor.appendChild(input)
         }
     }
