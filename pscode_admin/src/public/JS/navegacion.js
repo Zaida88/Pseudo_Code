8c877be458162b4d23fe56f441e2cb594c7ab8d8
Joaquin <joaquin050416@gmail.com>
2022-04-22 22:43:53 -0500

    Pequeña correcion a la navegacion

--- pscode_admin/src/public/JS/navegacion.js
+++ pscode_admin/src/public/JS/navegacion.js
@@ -1,11 +1,12 @@
-function mostrarMenu(){
+ function mostrarMenu(){
     console.log("Boton funcional");
-    document.getElementById("nav-bar").style.display= "flex";
-    document.getElementById("nav-bar").style.marginTop = "500px";
-    document.getElementById("menu__boton").style.display= "none";
-}
+     document.getElementById("nav-bar").style.display= "flex";
+     document.getElementById("nav-bar").style.marginTop = "500px";
+     document.getElementById("menu__boton").style.display= "none"; 
+ }
 function cerrarMenu(){
     console.log("Boton funcional");
     document.getElementById("nav-bar").style.display= "none";
     document.getElementById("menu__boton").style.display= "flex";
 }
+
