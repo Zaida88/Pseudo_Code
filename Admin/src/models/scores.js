8f74e506d62cb552288ac5ebe0c557002f753578
Liseth Ponce <lka.ponce@yavirac.edu.ec>
2022-09-12 15:23:45 -0500

    modelo de la tabla scores

---
+++ Admin/src/models/scores.js
@@ -0,0 +1,13 @@
+const scores = (sequelize,type) => {
+    return sequelize.define("scores",{
+        id_score: {
+            type: type.INTEGER,
+            primaryKey: true,
+            autoIncrement: true, 
+        },
+        score: type.INTEGER,
+    },{
+        timestamps: false,
+    })
+}
+module.exports=scores
