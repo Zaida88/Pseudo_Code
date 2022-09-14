1d710d5c7e4e3c30239e39c850dc4e2b8434275e
josueskt <77248411+josueskt@users.noreply.github.com>
2022-09-14 14:31:05 -0500

    1

--- Admin/src/public/js/menu.js
+++ Admin/src/public/js/menu.js
@@ -1,34 +1,5 @@
 
-var selec = null;
-function on_suim(e){
-event.preventDefault(); 
-var form_data = read_data();
-if(selec == null){
- ingreso_data(form_data);
-}
-}
 
-function read_data(){
-    var for_data = {}
-    for_data["name"] = document.getElementById("number").value;
-    for_data["code"] = document.getElementById("name").value;
-    return for_data;
-
-}
-function ingreso_data(data){
-var table = document.getElementById("tabla").getElementsByTagName("tbody")[0];
-var new_tabla = table.insertRow(table.length);
-var celda1 = new_tabla.insertCell(0)
-celda1.innerHTML = data.name
-var celda2 = new_tabla.insertCell(1)
-celda2.innerHTML = data.code
-var celda3 = new_tabla.insertCell(2)
-celda3.innerHTML = '<button>edit</button>'
-}
-function on_edit(td){
-  selec = td.parentElement.parentElement;
-  //document-getElementById("number").value = selec.celdas[0].innerHTML;
-}
 
 const $openclos = document.getElementById("Close-open"),
 $user_close = document.getElementById("close_secion")
@@ -39,7 +10,7 @@ $openclos.addEventListener("click",()=>{
 const $openclose = document.getElementById("open-close"),
 $asaid = document.getElementById("menu")
 $openclose.addEventListener("click",()=>{
-$asaid.classList.toggle("desplegar")
+$asaid.classList.toggle("deploy")
 })
 
 
