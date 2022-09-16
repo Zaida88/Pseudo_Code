063502b34d8de01431921c8a128fc9e623b9113f
josueskt <77248411+josueskt@users.noreply.github.com>
2022-09-16 08:54:10 -0500

    2

--- Admin/src/controllers/Menu.contoller.js
+++ Admin/src/controllers/Menu.contoller.js
@@ -1,7 +1,21 @@
+const photoUser = require("photo");
+
+const sql= require('../configuracion_bd/bd_sql')
+
 const menu = {};
 
 menu.user = (req, res, next) => {
   res.render('../views/partials/menu.hbs');
 }
+
+
+
+
+
+
+
+
+
+
 
 module.exports = menu;
