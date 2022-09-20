688b661576b5eca58297eb8ee7c75bfacbdfa630
fernandamarcillo <mfg.marcillo@yavirac.edu.ec>
2022-09-20 16:23:35 -0500

    test, question, answer

---
+++ Admin/src/controllers/questionController.js
@@ -0,0 +1,42 @@
+const question ={}
+
+const orm = require("../databaseConfiguration/bd_orm")
+const sql = require("../databaseConfiguration/bd_sql")
+
+question.show=(req,res)=>{
+    res.render("question")
+}
+question.send=async(req,res)=>{
+    const{question}=req.body
+    const newquestion={
+        question
+    }
+    await  orm.questions.create(newquestion)
+    req.flash("success","guardado exitosamente")
+    res.redirect("/question/list")
+}
+
+question.list=async(req,res)=>{
+    const list=await sql.query("select*from question")
+    res.render("question", {list})
+}
+question.bring=async(req,res)=>{
+    const id=req.params.id
+    const list=await sql.query("select*from questions where idQuestion=?", {id})
+    res.render("question",{list})
+}
+question.update=async(req,res)=>{
+    const id=req.params.id
+    const {question}=req.body
+    const newquestion={
+        question
+    }
+    await  orm.questions.findOne({where: {idQuestion: id}})
+    .then(updatequestion=>{
+        updatequestion.update(newquestion)
+    })
+        
+    req.flash("success","guardado exitosamente")
+    res.redirect("/question/list")
+}
+module.exports= question
--- Admin/src/controllers/testController.js
+++ Admin/src/controllers/testController.js
@@ -2,33 +2,41 @@ const test ={}
 
 const orm = require("../databaseConfiguration/bd_orm")
 const sql = require("../databaseConfiguration/bd_sql")
-const objectives = require("../models/objectivesModel")
 
-test.enviar = async (req, res) => {
-    const id = req.user.iduser
-    const { name, description, idTest, unic, numbers } =
-      req.body
-    const newtest = {
-      name,
-      description, 
-      userIduser: id,
+test.show=(req,res)=>{
+    res.render("test")
+}
+test.send=async(req,res)=>{
+    const{test}=req.body
+    const newtest={
+        test
     }
-    await orm.tests.create(newtest);
+    await  orm.tests.create(newtest)
+    req.flash("success","guardado exitosamente")
+    res.redirect("/test/list")
+}
 
-    if (parseInt(numbers) === 1) {
-      await sql.query(
-        "insert into test_detail (tests,testIdTest) values (?,?)", [unic, parseInt(idTest)]
-      ); 
-    } else {
-      if (parseInt(numbers) > 1) {
-        for (let i = 0; i < objectives.length; i++) {
-          await sql.query(
-            "insert into  test_detai (tests,testIdTest) values (?,?)", [objectives[i], parseInt(idTest)]
-          );
-        }
-      }
+test.list=async(req,res)=>{
+    const list=await sql.query("select*from tests")
+    res.render("test", {list})
+}
+test.bring=async(req,res)=>{
+    const id=req.params.id
+    const list=await sql.query("select*from tests where idTest=?", {id})
+    res.render("test",{list})
+}
+test.update=async(req,res)=>{
+    const id=req.params.id
+    const {test}=req.body
+    const newtest={
+        test
     }
-    req.flash("success", "Guardado Exitosamente");
-    res.redirect("/test/listar/" + id);
-  }
-  module.exports= test
+    await  orm.tests.findOne({where: {idTests: id}})
+    .then(updatetest=>{
+        updatetest.update(newtest)
+    })
+        
+    req.flash("success","guardado exitosamente")
+    res.redirect("/test/list")
+}
+module.exports= test
