80614150ef07384dbdd82c217197ecb5eed3ad4d
josueskt <77248411+josueskt@users.noreply.github.com>
2022-09-23 09:54:09 -0500

    1

--- Admin/src/controllers/profile.controll.js
+++ Admin/src/controllers/profile.controll.js
@@ -1,18 +1,16 @@
 
 const sql = require('../databaseConfiguration/db_sql')
 const orm = require('../databaseConfiguration/db_orm')
+const user = {}
+user.get_user = async (req, res, next) => {
+    const ids = req.params.id
+    const User = await sql.query('select idUser, username, photo, password from users where idUser = ?', [ids])
+    res.render('user_profile/profile_view', { User });
+};
 
-
-module.exports= {
-
-profile:function(req ,res){
-
-user.query('select idUser, username, photo from users ')
-
-
-
-}
+module.exports = user;
 
 
 
-}
+
+
