6876c3d067b3a53049a251d832ed631a49d97a5c
Jinson <am987406@gmail.com>
2022-09-30 14:51:31 -0500

    hiden div mensaje, eliminacion de archivo js

--- Admin/src/public/js/project/hidden.project.js
+++
@@ -1,9 +0,0 @@
-const div = document.querySelector("h5");
-
-document.querySelector(".hide").addEventListener("click", () => {
-  div.classList.add("div_hide");
-});
-
-document.querySelector(".show").addEventListener("click", () => {
-  div.classList.remove("div_hide");
-});
