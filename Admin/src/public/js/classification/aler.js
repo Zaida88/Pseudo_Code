52d520a0f941e54c3f5b82363f96cf59e3a398d1
karla <kaa.paez@yavirac.edu.ec>
2022-10-03 10:39:31 -0500

    diseños

---
+++ Admin/src/public/js/classification/aler.js
@@ -0,0 +1,24 @@
+function confirmar(id){
+  const swalWithBootstrapButtons = Swal.mixin({
+    customClass: {
+      confirmButton: 'btn btn-success',
+      cancelButton: 'btn btn-danger'
+    },
+    buttonsStyling: false
+  })
+  
+  swalWithBootstrapButtons.fire({
+    title: 'Are you sure?',
+    text: "You won't be able to revert this!",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonText: 'Yes, delete it!',
+    cancelButtonText: 'No, cancel!',
+    reverseButtons: true
+  }).then((result) => {
+    if (result.isConfirmed) {
+      window.location= '/classification/remove/'+id;
+    }
+  })
+}
+      
